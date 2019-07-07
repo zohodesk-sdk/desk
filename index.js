03da536fd99b1931734a45a43e2cb44a00564e03
Vimalesan <vimalesan.ceg@gmail.com>
2019-07-07 08:35:59 +0530

    cache added

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(
+    express.static(path.join(__dirname, 'public'), {
+      setHeaders(res) {
+        res.setHeader('Cache-Control', 'public,max-age=31536000,immutable');
+      }
+    })
+  )
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
